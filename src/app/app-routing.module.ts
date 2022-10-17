310887b9acd1e60715bc2a3dd13f427e339b4a08
Charitha <Charitha.Neelisetty@ivycomptech.com>
2022-10-17 19:51:49 +0530

    Components Assignment(Angular)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first', component: FirstComponentComponent},
+    {path:'second',component:SecondComponentComponent},
+{path:'third',component:ThirdComponentComponent},
+{path:'fourth',component:FourthComponentComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    FourthComponentComponent
   ],
   imports: [
     BrowserModule,
